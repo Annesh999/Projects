79352d67c7571b637e4ad429b2f21db27a16881f
Annesh999 <145187528+Annesh999@users.noreply.github.com>
2024-12-24 23:26:14 +0530

    Add files via upload

---
+++ EmailClone/index.js
@@ -0,0 +1,25 @@
+let val=document.querySelector('.main-content');
+for(let i=0 ;i<19;i++){
+
+    val.innerHTML=val.innerHTML+`  <div >
+          <div class="flex bg-[#FFFFFF]  cursor-pointer hover:drop-shadow-xl 	  justify-between border-t-2 items-center	px-5 p-1">
+            <div class="flex items-center gap-3">
+              <span class="material-symbols-outlined box center">
+                check_box_outline_blank</span
+              >
+              <span class="material-symbols-outlined star center">star</span>
+              <span>PW </span>
+            </div>
+            <div >
+              <p>
+                “importan mail for 2 step verification ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc...........
+              </p>
+            </div>
+            <p class="cont-mail-date">19:30</p>
+          </div>
+      
+                        
+            
+        
+      </div>`
+}
